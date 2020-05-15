657d35906e5005052d12162031c9393ccd7b2cd6
Nic Pearson <nic.s.pearson@gmail.com>
2020-05-15 14:14:53 +0200

    chore(): update deps

--- lib/src/middleware/http-context.middleware.d.ts
+++ lib/src/middleware/http-context.middleware.d.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
 import * as httpContext from 'express-http-context';
 export declare const httpContextMiddleware: typeof httpContext.middleware;
-export declare const requestIdHandler: (_: Request<import("express-serve-static-core").ParamsDictionary>, __: Response, next: NextFunction) => void;
+export declare const requestIdHandler: (_: Request, __: Response, next: NextFunction) => void;
 export declare function getRequestIdContext(): string | undefined;
